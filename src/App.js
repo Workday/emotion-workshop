83ce04718115cc6bb47f189ac350c3d2ef0c1456
manuel carrera <manuel.carrera@workday.com>
2019-06-18 20:49:38 -0600

    fix: update steps

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * Steps:
  * 1. Move the TODOS h1 into it's own separate component and call the file Header.js
  * 2. In that file, create a styled component. For reference: https://emotion.sh/docs/styled
- * 3. Delete the css for the h1 in App.css
+ * 3. Use the same css from App css to style the Header and then comment out the css from App.css
  * 3. Import Header component in App.js
  * 4. To Test your component styles, change some css property on it.
  * 
