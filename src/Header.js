e07d0425716329093de23fe6d020c14a245ae0e6
manuel carrera <manuel.carrera@workday.com>
2019-06-18 21:05:22 -0600

    task: solution to creating styled component

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const HeaderContainer = styled.h1`
+    position: absolute;
+	top: -155px;
+	width: 100%;
+	font-size: 100px;
+	font-weight: 100;
+	text-align: center;
+	color:blue;
+	-webkit-text-rendering: optimizeLegibility;
+	-moz-text-rendering: optimizeLegibility;
+	text-rendering: optimizeLegibility;
+`
+function Header() {
+    return ( <HeaderContainer>Todos</HeaderContainer>)
+  }
+
+export default Header
