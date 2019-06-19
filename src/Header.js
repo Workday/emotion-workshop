06a46dd3c58ccac12aa9982dc517ebcfd3df1545
manuel carrera <manuel.carrera@workday.com>
2019-06-18 21:05:22 -0600

    task: solution to creating styled component

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,20 @@
-/**
- * Steps:
- * 1. Move the TODOS h1 into it's own separate component and call the file Header.js
- * 2. In that file, create a styled component. For reference: https://emotion.sh/docs/styled
- * 3. Use the same css from App css to style the Header and then comment out the css from App.css
- * 3. Import Header component in App.js
- * 4. To Test your component styles, change some css property on it.
- * 
- */
-
 import React from 'react'
 import styled from '@emotion/styled'
 
-// create style component here and replace the h1 with your styled component
-
+const HeaderContainer = styled.h1`
+  position: absolute;
+  top: -155px;
+  width: 100%;
+  font-size: 100px;
+  font-weight: 100;
+  text-align: center;
+  color: blue;
+  -webkit-text-rendering: optimizeLegibility;
+  -moz-text-rendering: optimizeLegibility;
+  text-rendering: optimizeLegibility;
+`
 function Header() {
-    return ( <h1>Todos</h1>)
-  }
+  return <HeaderContainer>Todos</HeaderContainer>
+}
 
 export default Header
